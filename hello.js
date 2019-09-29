c20bbc09f226889cb5c7f82377ede18b01f91c19
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-09-29 17:34:20 +0900

    fix project structure

--- hello.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function hello() {
-    return 'hello';
-}
-exports.hello = hello;
-console.log(hello());
---
+++ src/hello.ts
@@ -0,0 +1,3 @@
+export function hello(arg: any) {
+    return arg
+}
